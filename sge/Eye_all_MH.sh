1c9bf6dbda60f731a9e27738b7dda58724e6c16d
Gary Macindoe <gary@garymacindoe.co.uk>
2013-10-02 12:57:05 +0100

    Working SGE scripts for Stochastic Eye models.

--- sge/Eye_all_MH.sh
+++ sge/Eye_all_MH.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#$ -l h_vmem=1G,tmem=1G
+#$ -l h_vmem=4G,tmem=4G
+#$ -R y
+#$ -pe orte 25
+#$ -l 10g=yes
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
@@ -10,5 +13,5 @@ export PATH=${HOME}/bin:${MATLAB_ROOT}/bin/${MATLAB_ARCH}:${MPI_ROOT}/bin:${PATH
 export LD_LIBRARY_PATH=${HOME}/lib64:${MPI_ROOT}/lib:${MATLAB_ROOT}/bin/${MATLAB_ARCH}:/opt/gridengine/lib/lx26-amd64
 
 
-${MPI_ROOT}/bin/mpirun -n ${NSLOTS} ./Eye_all_MH ${INPUT_DATA} ${DATASET} --num_temperatures=100 --num_burnin_samples=5000 --num_posterior_samples=50000 results/${OUTPUT_FMT}_${NSLOTS}
+${MPI_ROOT}/bin/mpirun --mca btl_tcp_if_exclude lo,eth0 -n ${NSLOTS} ./Eye_all_MH ${INPUT_DATA} ${DATASET} --num_temperatures=100 --num_burn_in_samples=5000 --num_posterior_samples=50000 results/${OUTPUT_FMT}_${NSLOTS}
 exit 0
